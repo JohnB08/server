d66be44da0c79ed134701e3ec5af18377748dffe
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-19 13:05:06 +0000

    errorhandling of env and comments

--- db/db.ts
+++ db/db.ts
@@ -2,6 +2,24 @@ import pkg from  "pg"
 
 const {Pool} = pkg
 
+const envFileVerifier = (envFile:string | undefined) =>{
+    if (typeof envFile === "string") return envFile
+    else return undefined
+}
+
+const envFilesArray = [process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, process.env.DATABASE_HOST, process.env.DATABASE_PORT, process.env.DATABASE_NAME]
+const errorArray: any[] = []
+
+const verifyAllEnv = () =>{
+    envFilesArray.forEach(env=>{
+        envFileVerifier(env) ? env : errorArray.push(new Error(`Issues with env file ${env}`))
+    })
+}
+
+verifyAllEnv()
+
+console.log(errorArray)
+
 export const pool = new Pool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
--- server.ts
+++ server.ts
@@ -14,43 +14,69 @@ const port = 3000
 app.use(express.json())
 app.use(cors())
 
+
+/* Skjekker om postgres serveren har korrekte tables, bruker gratisversjonen av render som
+av og til wiper serveren. */
 await initializeServer(difficultyOptions)
 
+
+/* Endpoint for å sette eller oppdatere highscore. */
 app.post("/setscore", async (req, res)=>{
     const body = req.body
+
+    /* Hvis body mangler data, returner status 401 missing data. */
     if (!validateBody(body)){
        return res.status(401).json({message: "Missing Username/Highscore"})
     }
+
+    /* Destrukturerer body og saniterer username */
     const {username, highscore, difficulty} = body
     let sanitUsername = usernameChecker(username)
+
+    /* Skjekker om brukernavnet finnes fra før. */
     const existingUsername = await fetchUsername(sanitUsername, difficulty)
     console.log(existingUsername)
+
+    /* Hvis postgres serveren gir error, send 500 error til client.  */
     if (!existingUsername.success){
+        console.log(existingUsername.error)
         return res.status(500).json({message: "Internal Server Error"})
     }
+
+    /* Hvis brukernavnet ikke finnes, prøv å post en ny bruker. */
     if (existingUsername.success && existingUsername.data?.rowCount === 0){
         const setHighscore = await postNewUser(sanitUsername, highscore, difficulty)
         console.log(setHighscore)
+
+        /* Hvis server error, send 500 status. hvis success send 200 status. */
         if (!setHighscore.success){
+            console.log(setHighscore.error)
             return res.status(500).json({message: "Internal Server Error"})
         } return res.status(200).json({message: "Highscore Set!"})
     }
+
+    /* Hvis brukernavnet allerede finnes, prøv å oppdater highscore til navn. */
     if (existingUsername.success && existingUsername.data?.rowCount){
         const update = await updateHighScore(sanitUsername, highscore, difficulty)
         console.log(update)
+
+        /* Hvis servererror, send 500 status, hvis ikke send 200 status. */
         if (!update.success){
+            console.log(update.error)
             return res.status(500).json({message: "Internal Server Error"})
         } return res.status(200).json({message: "Highscore Set!"})
     }
 })
 
 
-
+/* Endpoint for å fetche highscore for set difficulty. */
 app.post("/highscore", async (req,res)=>{
     const {difficulty} = req.body
     const data = await fetchTopTen(difficulty)
     console.log(data)
+    /* Hvis server fail, send 500 error, hvis ikke send 200 status + data. */
     if (!data.success){
+        console.log(data.error)
         return res.status(500).json({message: "Internal Server Error"})
     } return res.status(200).json({message: "Success!", data: data.data?.rows})
 })
@@ -59,7 +85,7 @@ app.post("/highscore", async (req,res)=>{
 
 
 
-
+/* App lytter til standard ip + port. */
 app.listen(port, ()=>{
     console.log("server running")
 })

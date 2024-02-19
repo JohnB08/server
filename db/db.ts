2bdf9fba13abc620b9531d27a8c3041308cf6a34
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-19 13:18:46 +0000

    changed verifier to boolean instead of truthy

--- db/db.ts
+++ db/db.ts
@@ -3,8 +3,8 @@ import pkg from  "pg"
 const {Pool} = pkg
 
 const envFileVerifier = (envFile:string | undefined) =>{
-    if (typeof envFile === "string" && envFile.length > 0) return envFile
-    else return undefined
+    if (typeof envFile === "string" && envFile.length > 0) return true
+    else return false
 }
 
 const envFilesArray = [{DATABASE_USER: process.env.DATABASE_USER}, {DATABASE_PASSWORD: process.env.DATABASE_PASSWORD}, {DATABASE_HOST: process.env.DATABASE_HOST}, {DATABASE_PORT: process.env.DATABASE_PORT}, {DATABASE_NAME: process.env.DATABASE_NAME}]
